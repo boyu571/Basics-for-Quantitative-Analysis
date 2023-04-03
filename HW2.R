1762ed4e49df55db3408dfbd4f2e8d2d1a36f4d2
Ryeo Myung Kang <boyu571@gmail.com>
2023-04-03 22:57:50 +0900

    file modified

--- HW2.R
+++ HW2.R
@@ -5,6 +5,20 @@ library()
 dataxls = as_tibble(read_excel("401ksubs.xls", col_names = c("e401k", "inc", "marr",  "male", "age",  "fsize",  "nettfa", "p401k","pira", "incsq", "agesq")))
 dataxls
 
+# 1-1
 subdata <- dataxls %>% filter(marr==1 & fsize==2)
+count(subdata)
 
+# 1-2
+ols = lm(nettfa ~ inc + age, data=subdata)
+summary(ols)
 
+# 1-3
+confint(ols, 'age', 0.95)
+confint(ols, 'inc', 0.95)
+
+# 1-5
+ols2 = lm(I(nettfa - age) ~ inc + age, data=subdata)
+summary(ols2)
+
+qt(0.99,(1491))
